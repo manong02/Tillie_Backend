a931c9f9c600c059b0e47523d077f4c1d58ca612
manong02 <manon.golos@gmail.com>
2025-08-03 19:16:12 +0200

    feat: initialize Django project with authentication, shop, inventory, and orders apps

--- tillie_backend/settings.py
+++ tillie_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-y8+v%87!#lk3n1edxl*771)^9im*q)^!q6b*gn!iptwyv#1$6w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
